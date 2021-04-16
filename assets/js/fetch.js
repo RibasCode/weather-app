ca61b8bea1e52a8a59db1af9c555555da2e5a636
Marc Ribas <ribascode@gmail.com>
2021-04-16 10:56:52 +0200

    search btn connects API dynamically

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -3,27 +3,38 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-const btn = document.getElementById('primary-btn')
-const contenido = document.querySelector('#contenido')
 
 
-const fetchData = async () => {
-    try{
-        const proxy = `http://cors-anywhere.herokuapp.com/`
-        const key = `136882aa473e6f9a1455a8308bb03c07`
-        var city = `barcelona`
 
-        const respuesta = await fetch(`${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}&units=metric`)
-        const data = await respuesta.json()
-        console.log(data)
+const serachCity = document.getElementById('search-city')
 
-    }catch(error){
-        console.log(error);
-    }
-
-}
+    serachCity.addEventListener('click', () => {
 
 
-fetchData();
+        const fetchData = async () => {
+            try{
 
+                const proxy = `http://cors-anywhere.herokuapp.com/`
+                const key = `136882aa473e6f9a1455a8308bb03c07`
+                let city = document.getElementById('city-input').value
+
+                const respuesta = await fetch(`${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}&units=metric`)
+                const data = await respuesta.json()
+                console.log(data)
+        
+            }catch(error){
+                console.log(error);
+            }
+        
+        }
+        
+        fetchData();
+
+    })
+
+
+
+    
 })
+
+
