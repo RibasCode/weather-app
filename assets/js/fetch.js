38bda517e2541769024808cb31ce44f89a4cd09f
Marc Ribas <ribascode@gmail.com>
2021-04-24 21:37:23 +0200

    implement dark functionality

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -37,13 +37,52 @@ const cardImg = document.getElementById('card__img');
 
                 if(data.weather[0].main == 'Clear' && data.weather[0].icon.includes('d')){
                     cardImg.src = 'assets/img/sunny.png'
+                    document.documentElement.style.setProperty('--clr-primary', '#ffffff');
+                    document.documentElement.style.setProperty('--clr-typo', '#1f1f1f');
+                    document.documentElement.style.setProperty('--clr-border', '#eaeaea');
+                    document.documentElement.style.setProperty('--clr-outline', '#d1d1d1');
+                    document.documentElement.style.setProperty('--clr-black', '#000000');
+                    document.documentElement.style.setProperty('--clr-white', '#ffffff');
+                    document.documentElement.style.setProperty('--clr-broken-white', '#fafafa');
+                    document.documentElement.style.setProperty('--clr-broken-black', '#1f1f1f');
+                    document.documentElement.style.setProperty('--clr-shadow', 'rgba(0,0,0, 0.05)');
+                    document.documentElement.style.setProperty('--clr-shadow-hover', 'rgba(0,0,0, 0.10)');
                 }else if(data.weather[0].main == 'Clouds' && data.weather[0].icon.includes('d')){
                     cardImg.src = 'assets/img/clouds.png'
+                    document.documentElement.style.setProperty('--clr-primary', '#ffffff');
+                    document.documentElement.style.setProperty('--clr-typo', '#1f1f1f');
+                    document.documentElement.style.setProperty('--clr-border', '#eaeaea');
+                    document.documentElement.style.setProperty('--clr-outline', '#d1d1d1');
+                    document.documentElement.style.setProperty('--clr-black', '#000000');
+                    document.documentElement.style.setProperty('--clr-white', '#ffffff');
+                    document.documentElement.style.setProperty('--clr-broken-white', '#fafafa');
+                    document.documentElement.style.setProperty('--clr-broken-black', '#1f1f1f');
+                    document.documentElement.style.setProperty('--clr-shadow', 'rgba(0,0,0, 0.05)');
+                    document.documentElement.style.setProperty('--clr-shadow-hover', 'rgba(0,0,0, 0.10)');
                 }else if(data.weather[0].main == 'Clear' && data.weather[0].icon.includes('n')){
                     cardImg.src = 'assets/img/moon.png'
-                    document.documentElement.style.setProperty('--darkColor', '#000');
+                    document.documentElement.style.setProperty('--clr-primary', '#282828');
+                    document.documentElement.style.setProperty('--clr-typo', '#fafafa');
+                    document.documentElement.style.setProperty('--clr-border', '#404040');
+                    document.documentElement.style.setProperty('--clr-outline', '#808080');
+                    document.documentElement.style.setProperty('--clr-black', '#ffffff');
+                    document.documentElement.style.setProperty('--clr-white', '#282828');
+                    document.documentElement.style.setProperty('--clr-broken-white', '#1f1f1f');
+                    document.documentElement.style.setProperty('--clr-broken-black', '#fafafa');
+                    document.documentElement.style.setProperty('--clr-shadow', 'rgba(0,0,0, 0.20)');
+                    document.documentElement.style.setProperty('--clr-shadow-hover', 'rgba(0,0,0, 0.40)');
                 }else if(data.weather[0].main == 'Clouds' && data.weather[0].icon.includes('n')){
                     cardImg.src = ''
+                    document.documentElement.style.setProperty('--clr-primary', '#282828');
+                    document.documentElement.style.setProperty('--clr-typo', '#fafafa');
+                    document.documentElement.style.setProperty('--clr-border', '#404040');
+                    document.documentElement.style.setProperty('--clr-outline', '#808080');
+                    document.documentElement.style.setProperty('--clr-black', '#ffffff');
+                    document.documentElement.style.setProperty('--clr-white', '#282828');
+                    document.documentElement.style.setProperty('--clr-broken-white', '#1f1f1f');
+                    document.documentElement.style.setProperty('--clr-broken-black', '#fafafa');
+                    document.documentElement.style.setProperty('--clr-shadow', 'rgba(0,0,0, 0.20)');
+                    document.documentElement.style.setProperty('--clr-shadow-hover', 'rgba(0,0,0, 0.40)');
                 }
 
             }catch(error){
