22c0f3d692e1c8bfd9d615ec290892b9beacf579
Marc Ribas <ribascode@gmail.com>
2021-05-08 19:34:27 +0200

    clean app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,24 +9,28 @@
 const citySearchBtn = document.getElementById('city-search-btn');
 const cardCity = document.getElementById('card__city');
 const cardWeather = document.getElementById('card__weather');
-const cardTime = document.getElementById('card__time');
-const cardDay = document.getElementById('card__day');
 const cardTemperature = document.getElementById('card__temperature');
 const cardMaxTemperature = document.getElementById('card__max-temperature');
 const cardMinTemperature = document.getElementById('card__min-temperature');
 const cardHumidity = document.getElementById('card__humidity');
 const cardWind = document.getElementById('card__wind');
 const cardImg = document.getElementById('card__img');
+const heroBot = document.getElementById('hero__bot');
+const cardTime = document.getElementById('card__time');
+const cardDay = document.getElementById('card__day');
 const cardContent = document.getElementById('card__content');
 const cardGreetingsText = document.getElementById('card__greetings-text');
 const cardGreetings = document.getElementById('card__greetings');
 const cardError = document.getElementById('card__error');
-const heroBot = document.getElementById('hero__bot');
+
+let palanca = true;
+
+// --------------------------------------------------------------
+// class inst
+// --------------------------------------------------------------
 
 const card = new Card()
 const fetchRequest = new Fetch()
-
-let palanca = true;
 
 // --------------------------------------------------------------
 // press enter key functionality
